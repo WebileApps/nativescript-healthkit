cc84069666eefcbf3885d958f349f2bd837f931c
Rajiv Narayana <rajiv@webileapps.com>
2020-01-07 19:04:13 +0530

    Throw error when there are no permissions.

--- src/healthkit.ios.ts
+++ src/healthkit.ios.ts
@@ -1,5 +1,5 @@
 
-declare const WAHealthDataManager: any, HKHealthStore : any; 
+declare const WAHealthDataManager: any, HKHealthStore : any, HKAuthorizationStatus: any;
 
 const LastSyncKey = "WAHealthKitLastSyncTimeStamp";
 
@@ -8,6 +8,10 @@ export function getHealthDataSince(date: Date) : Promise<any> {
         return Promise.reject(new Error("Need a since date to fetch information"));
     } else {
         console.log("Fetching data from "+date.toDateString());
+    }
+    
+    if (!canRequestAccess()) {
+        return Promise.reject(new Error("We don't have access to your HealthKit data. Please grant us access from Settings app"));
     }
     return new Promise((resolve, reject) => {
         WAHealthDataManager.shared().getHealthDataSinceWithCompletionHandler(date, (response, error) => {
@@ -25,6 +29,25 @@ export function getHealthDataSince(date: Date) : Promise<any> {
         })
     })
 }
+
+export function canRequestAccess() : boolean {
+    const healthStore = new HKHealthStore();
+    return healthStore.authorizationStatusForType(HKObjectType.workoutType()) != HKAuthorizationStatus.HKAuthorizationStatusSharingDenied &&
+    healthStore.authorizationStatusForType(HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)) != HKAuthorizationStatus.HKAuthorizationStatusSharingDenied;
+}
+
+/**
+ * 
+[HKObjectType workoutType],
+[HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRate],
+[HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount],
+[HKObjectType quantityTypeForIdentifier: HKQuantityTypeIdentifierActiveEnergyBurned],
+[HKObjectType quantityTypeForIdentifier: HKQuantityTypeIdentifierAppleExerciseTime],
+[HKObjectType quantityTypeForIdentifier: HKQuantityTypeIdentifierDistanceWalkingRunning],
+[HKObjectType quantityTypeForIdentifier: HKQuantityTypeIdentifierDistanceWheelchair],
+[HKObjectType quantityTypeForIdentifier: HKQuantityTypeIdentifierDistanceSwimming],
+[HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierDistanceCycling]
+ */
 
 export function getHealthDataSinceLastSync() : Promise<any> {
     let date : Date = getLastSyncedAt();
